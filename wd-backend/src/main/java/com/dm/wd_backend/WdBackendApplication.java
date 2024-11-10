23b6bf3a465a06ed05cd5e322200b6466d0b6d6d
David Morgan <demorgan94@gmail.com>
2024-11-10 00:32:59 -0600

    feat: add springboot backend project and common properties

---
+++ wd-backend/src/main/java/com/dm/wd_backend/WdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dm.wd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdBackendApplication.class, args);
+	}
+
+}
---
+++ wd-backend/src/test/java/com/dm/wd_backend/WdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dm.wd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
