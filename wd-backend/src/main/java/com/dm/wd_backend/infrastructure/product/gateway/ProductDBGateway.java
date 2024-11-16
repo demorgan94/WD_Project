fd19b4f787d66091a4bc6b65958228e1852601b2
David Morgan <demorgan94@gmail.com>
2024-11-15 23:16:04 -0600

    fix: implement ProductGateway changes

--- wd-backend/src/main/java/com/dm/wd_backend/infrastructure/product/gateway/ProductDBGateway.java
+++ wd-backend/src/main/java/com/dm/wd_backend/infrastructure/product/gateway/ProductDBGateway.java
@@ -1,49 +1,71 @@
 package com.dm.wd_backend.infrastructure.product.gateway;
 
+import com.dm.wd_backend.domain.product.exceptions.ProductNotFoundException;
 import com.dm.wd_backend.domain.product.gateway.ProductGateway;
 import com.dm.wd_backend.domain.product.model.Product;
 import com.dm.wd_backend.infrastructure.config.database.entities.ProductEntity;
 import com.dm.wd_backend.infrastructure.config.database.repositories.ProductRepository;
-import org.springframework.stereotype.Repository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
+@Service
+@RequiredArgsConstructor
 public class ProductDBGateway implements ProductGateway {
     private final ProductRepository productRepository;
 
-    public ProductDBGateway(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
+    /**
+     * Saves the given product to the database.
+     *
+     * @param product the product to be saved
+     * @return the saved product with any database-generated values populated
+     */
     @Override
-    public Product create(Product product) {
+    public Product save(Product product) {
+        if (product.getId() != null) {
+            return this.findById(product.getId())
+                    .orElseThrow(() -> new ProductNotFoundException(product.getId()))
+                    .updateProduct(product);
+        }
         return this.productRepository.save(ProductEntity.fromDomain(product)).toDomain();
     }
 
+    /**
+     * Deletes the product with the given id from the database.
+     *
+     * @param id the id of the product to be deleted
+     * @return true if the product was successfully deleted, false if no product was found with the given id
+     */
     @Override
-    public Product update(Product product) {
-        return this.productRepository.save(ProductEntity.fromDomain(product)).toDomain();
-    }
-
-    @Override
-    public void delete(UUID id) {
+    public boolean delete(UUID id) {
         productRepository.deleteById(id);
+        return this.findById(id).isEmpty();
     }
 
+    /**
+     * Retrieves the product with the given id from the database.
+     *
+     * @param id the id of the product to be retrieved
+     * @return an Optional containing the product with the given id, or an empty Optional if no product was found with that id
+     */
     @Override
     public Optional<Product> findById(UUID id) {
-        return productRepository.findById(id)
+        return this.productRepository.findById(id)
                 .map(ProductEntity::toDomain);
     }
 
+    /**
+     * Retrieves all products from the database.
+     *
+     * @return a list of all products in the database
+     */
     @Override
     public List<Product> findAll() {
         return productRepository.findAll()
                 .stream()
-                .map(ProductEntity::toDomain)
-                .toList();
+                .map(ProductEntity::toDomain).toList();
     }
 }
